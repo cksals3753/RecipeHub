45f897707d39b81908a0e42352f440923d35d986
JeonChanMin <cksals3753@naver.com>
2022-02-21 21:30:55 +0900

    Feat: 탭

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import Title from './components/Title';
 import Search from './components/Search';
+import Logo from './components/logo';
+//import Add from './components/add';
+//import MySearchBar from './components/SearchBar';
 
-function App() {
+const menuList = {
+  0: <Mail />,
+  1: <Cafe />,
+};
+
+function App(props) {
+
+  super();
+
+  this.state = {
+    menu: 0,
+  };
+
+  changeMenu = (menuIndex) => {
+    this.setState({ menu: menuIndex });
+  }
   return (
-    <div>
+    <div className="wrap">
       <Title />
-      <Search />
+      <div className="menuBar">
+        <ul className="tabs">
+          <li className={`${this.state.menu === 0 ? 'active' : ''}`} onClick={() => this.changeMenu(0)}>Mail</li>
+          <li className={`${this.state.menu === 1 ? 'active' : ''}`} onClick={() => this.changeMenu(1)}>Cafe</li>
+        </ul>
+      </div>
+      <div className="contentArea">
+        {menuList[this.state.menu]}
+      </div>
     </div>
-  );
+  )
+
 }
+
+
 
 export default App;
